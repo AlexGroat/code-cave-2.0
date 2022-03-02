a8b86358f0df66283bf822f5910bb319046f3f9f
Alex <alex.groat.95@gmail.com>
2022-03-03 10:24:31 +1030

    old home layout before convert to chakra wrap

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,19 +1,26 @@
 import Sidebar from "../../components/Sidebar";
-import { Flex, Text, IconButton } from "@chakra-ui/react";
-import { FiMenu } from "react-icons/fi";
+import { Flex, Heading } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <div>
-    <Flex w="100%">
-      <Sidebar />
-      <Flex
-        pos="absolute"
-        top="50%"
-        left="50%"
-        transform="translate(-50%, -50%)"
-      ></Flex>
-    </Flex>
+    <div className="home-container" style={{display: "flex"}}>
+      <div className="column-1 col-2">
+        <Flex w="100%">
+          <Sidebar />
+          <Flex
+            pos="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+          ></Flex>
+        </Flex>
+      </div>
+      <div className="column-2 col-5">
+        <Heading mt={2}>Code Cave</Heading>
+      </div>
+      <div className="column-3 col-5" >
+        <Heading mt={2}>News here</Heading>
+      </div>
     </div>
   );
 }
