cd1a06c7fdb15195821dc9b6ff1010a85aa806cc
Alex Groat <alex.groat.95@gmail.com>
2022-03-02 12:49:38 +1030

    still work on nav but add user mutations

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -3,7 +3,16 @@ import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import Auth from "../utils/auth";
-import { Flex, IconButton, Divider, Avatar, Heading } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  IconButton,
+  Divider,
+  Avatar,
+  Heading,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import {
   FiMenu,
   FiHome,
@@ -12,6 +21,7 @@ import {
   FiDollarSign,
   FiBriefcase,
   FiSettings,
+  FiUserPlus,
 } from "react-icons/fi";
 import { IoPawOutline } from "react-icons/io5";
 import NavItem from "./NavItem";
@@ -21,6 +31,8 @@ export default function Sidebar() {
     event.preventDefault();
     Auth.logout();
   };
+  const { toggleColorMode } = useColorMode();
+  const formBackground = useColorModeValue("gray.100", "gray.700");
 
   const { username: currentUser } = useParams();
 
@@ -59,18 +71,52 @@ export default function Sidebar() {
             else changeNavSize("small");
           }}
         />
-        <NavItem
-          navSize={navSize}
-          icon={FiHome}
-          title="Dashboard"
-          description="This is the description for the dashboard."
-        />
-        <NavItem navSize={navSize} icon={FiCalendar} title="Profile" active />
-        <NavItem navSize={navSize} icon={FiUser} title="Clients" />
-        <NavItem navSize={navSize} icon={IoPawOutline} title="Animals" />
-        <NavItem navSize={navSize} icon={FiDollarSign} title="Stocks" />
-        <NavItem navSize={navSize} icon={FiBriefcase} title="Reports" />
-        <NavItem navSize={navSize} icon={FiSettings} title="Settings" />
+        <Link to="/">
+          <NavItem
+            navSize={navSize}
+            icon={FiHome}
+            title="Dashboard"
+            description="This is the description for the dashboard."
+          />
+        </Link>
+        {Auth.loggedIn() ? (
+          <>
+            <Link to="/">
+              <NavItem navSize={navSize} icon={FiCalendar} title="" active />
+            </Link>
+
+            <Flex
+              p="5%"
+              flexDir="column"
+              w="100%"
+              alignItems={navSize == "small" ? "center" : "flex-start"}
+              mb={4}
+            >
+              <Divider display={navSize == "small" ? "none" : "flex"} />
+              <Flex mt={4} align="center">
+                <Avatar size="sm" src="avatar-1.jpg" />
+                <Flex
+                  flexDir="column"
+                  ml={4}
+                  display={navSize == "small" ? "none" : "flex"}
+                >
+                  <Heading as="h3" size="sm">
+                    Welcome, {Auth.getProfile().data.username}
+                  </Heading>
+                </Flex>
+              </Flex>
+            </Flex>
+          </>
+        ) : (
+          <>
+            <Link to="/login">
+              <NavItem navSize={navSize} icon={FiUser} title="Login" />
+            </Link>
+            <Link to="/Signup">
+              <NavItem navSize={navSize} icon={FiUserPlus} title="Signup" />
+            </Link>
+          </>
+        )}
       </Flex>
 
       <Flex
@@ -82,15 +128,12 @@ export default function Sidebar() {
       >
         <Divider display={navSize == "small" ? "none" : "flex"} />
         <Flex mt={4} align="center">
-          <Avatar size="sm" src="avatar-1.jpg" />
           <Flex
             flexDir="column"
             ml={4}
             display={navSize == "small" ? "none" : "flex"}
           >
-            <Heading as="h3" size="sm">
-              Sylwia Weller
-            </Heading>
+            <Button onClick={toggleColorMode}>Toggle Theme</Button>
           </Flex>
         </Flex>
       </Flex>
