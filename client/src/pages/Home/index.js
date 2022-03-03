8ba04e510d37196c39d1a57150aeccc413d1a74b
Alex <alex.groat.95@gmail.com>
2022-03-03 12:56:27 +1030

    home middle and right column wrap at 800px

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,11 @@
 import Sidebar from "../../components/Sidebar";
 import { Flex, Heading } from "@chakra-ui/react";
+import './index.css';
 
 export default function Home() {
   return (
     <div className="home-container" style={{display: "flex"}}>
-      <div className="column-1 col-2">
+      <div className="column-1 col-2" style={{position: 'relative', zIndex: '1'}}>
         <Flex w="100%">
           <Sidebar />
           <Flex
@@ -15,11 +16,15 @@ export default function Home() {
           ></Flex>
         </Flex>
       </div>
-      <div className="column-2 col-5">
+      <div className="column-container">
+      <div className="columns" style={{display: 'flex'}}>
+      <div className="column-2 col-6">
         <Heading mt={2}>Code Cave</Heading>
       </div>
-      <div className="column-3 col-5" >
+      <div className="column-3 col-6">
         <Heading mt={2}>News here</Heading>
+      </div>
+      </div>
       </div>
     </div>
   );
