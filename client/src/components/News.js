6231becfc78573344a5b83b1101439ea7694ff5c
Alex <alex.groat.95@gmail.com>
2022-03-03 14:43:30 +1030

    references to news url works

--- client/src/components/News.js
+++ client/src/components/News.js
@@ -1,38 +1,38 @@
 import { useQuery } from "@apollo/client";
 import React from "react";
 import { LinkBox, Box, Heading, Spinner } from "@chakra-ui/react";
-import { GET_NEWS } from '../utils/queries';
+import { GET_NEWS } from "../utils/queries";
 
 const NewsBar = () => {
-    const { loading, data } = useQuery(GET_NEWS);
-    const news = data?.articles || [];
-    console.log(news);
+  const { loading, data } = useQuery(GET_NEWS);
+  const news = data?.articles || [];
+  console.log(news);
 
-    return (
+  return (
+    <div>
+      {loading ? (
         <div>
-        
-        {loading ? (
-          <div>Loading news <Spinner /></div>
-        ) : (
-          news.slice(0, 6).map((article) => (
-            <LinkBox maxW='sm' p='5' borderWidth='1px' rounded='md' mb={2}>
-              <div>
-                  <Heading size='md'>{article.author}</Heading>
-                <Box>
-                  <p >{article.title}</p>
-                </Box>
-                <span className="read-more">
-                  Read more{" "}
-                  <a href={article.url} target="_blank" style={{textDecoration: 'none'}}>
-                    here
-                  </a>
-                </span>
-              </div>
-            </LinkBox>
-          ))
-        )}
-      </div>
-    )
-}
+          Loading news <Spinner />
+        </div>
+      ) : (
+        news.slice(0, 6).map((article) => (
+          <LinkBox maxW="sm" p="5" borderWidth="1px" rounded="md" mb={2}>
+            <div>
+              <Heading size="md">{article.author}</Heading>
+              <Box>
+                <p>{article.title}</p>
+              </Box>
+              <Box as="a" color="teal.400" href="#" fontWeight="bold">
+                 <a href={article.url} target="_blank">Read More Here</a>
+              </Box>
+            </div>
+          </LinkBox>
+        ))
+      )}
+    </div>
+  );
+};
 
 export default NewsBar;
+
+
