6e8b3ccb7eddb32304aee12f13c41af19c5fbf12
Alex Groat <alex.groat.95@gmail.com>
2022-03-15 12:03:53 +1030

    comment form and singlepost

---
+++ client/src/pages/SinglePost/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+
+import { QUERY_SINGLE_POST } from '../../utils/queries';
+import { Heading } from "@chakra-ui/react";
+
+const SinglePost = () => {
+    // use useparams() to retrieve value of the route parameter postId in app.js
+
+    const { postId } = useParams();
+
+    const { loading, data } = useQuery(QUERY_SINGLE_POST, {
+        // pass url parameter
+        variables: { postId: postId },
+    });
+
+    const post = data?.post || {};
+
+    if (loading) {
+        return <Heading>Retrieving Post</Heading>
+    }
+
+    return (
+        <></>
+    )
+}
+
+export default SinglePost;
