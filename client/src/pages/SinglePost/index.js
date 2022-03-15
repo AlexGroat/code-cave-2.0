3cba78809036da881d53c1575f2cb17ab7e4673c
Alex Groat <alex.groat.95@gmail.com>
2022-03-15 12:47:14 +1030

    singlepost renders need to add comment form and comment list

--- client/src/pages/SinglePost/index.js
+++ client/src/pages/SinglePost/index.js
@@ -1,59 +1,91 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 
-import { QUERY_SINGLE_POST } from '../../utils/queries';
-import { Button, Flex, Heading, Text } from "@chakra-ui/react";
-import { Spinner } from "react-bootstrap";
+import { QUERY_SINGLE_POST } from "../../utils/queries";
+import {
+  Button,
+  Flex,
+  Heading,
+  Text,
+  Box,
+  Divider,
+  Spinner,
+  Code,
+} from "@chakra-ui/react";
 
-import Sidebar from '../../components/Sidebar';
-import News from '../../components/News';
+import Sidebar from "../../components/Sidebar";
+import News from "../../components/News";
 
 const SinglePost = () => {
-    // use useparams() to retrieve value of the route parameter postId in app.js
+  // use useparams() to retrieve value of the route parameter postId in app.js
 
-    const { postId } = useParams();
+  const { postId } = useParams();
 
-    const { loading, data } = useQuery(QUERY_SINGLE_POST, {
-        // pass url parameter
-        variables: { postId: postId },
-    });
+  const { loading, data } = useQuery(QUERY_SINGLE_POST, {
+    // pass url parameter
+    variables: { postId: postId },
+  });
 
-    const post = data?.post || {};
+  const post = data?.post || {};
 
-    if (loading) {
-        return <Heading>Retrieving Post<Spinner size='xs'/></Heading>
-    }
-
+  if (loading) {
     return (
-        <div className="home-container" style={{ display: "flex" }}>
-        <div
-          className="column-1 col-2"
-          style={{ position: "relative", zIndex: "1" }}
-        >
-          <Flex w="100%">
-            <Sidebar zIndex='1' />
-            <Flex
-              pos="absolute"
-              top="50%"
-              left="50%"
-              transform="translate(-50%, -50%)"
-            ></Flex>
-          </Flex>
-        </div>
-        <div className="column-container">
-          <div className="columns" style={{ display: "flex" }}>
-            <div className="column-2 col-6">
-           
-            </div>
-            <div className="column-3 col-6">
-            <Heading mt={3} mb={3}>Tech News</Heading>
-              <News />
-            </div>
+      <Heading>
+        Retrieving Post
+        <Spinner size="xs" />
+      </Heading>
+    );
+  }
+
+  return (
+    <div className="home-container" style={{ display: "flex" }}>
+      <div
+        className="column-1 col-2"
+        style={{ position: "relative", zIndex: "1" }}
+      >
+        <Flex w="100%">
+          <Sidebar zIndex="1" />
+          <Flex
+            pos="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+          ></Flex>
+        </Flex>
+      </div>
+      <div className="column-container">
+        <div className="columns" style={{ display: "flex" }}>
+          <div className="column-2 col-6">
+            <Box
+              borderWidth="1px"
+              borderRadius="lg"
+              overflow="hidden"
+              mb={2}
+              mt={5}
+            >
+              <Link to={`/profiles/${post?.postAuthor}`}>
+                <Heading ml={1}>{post?.postAuthor}</Heading>
+              </Link>
+
+              <Text ml={1}>{post?.createdAt}</Text>
+              <Divider />
+              <Code mt={2} mb={2} ml={1} fontSize="md">
+                {post?.postText}
+              </Code>
+              <Divider />
+            </Box>
+          </div>
+          <div className="column-3 col-6">
+            <Heading mt={3} mb={3}>
+              Tech News
+            </Heading>
+            <News />
           </div>
         </div>
       </div>
-    )
-}
+    </div>
+  );
+};
 
 export default SinglePost;
